19013beb30dfe98eeeb919b7a3e6eb7d68797edf
Wetwhale453 <107708661+Wetwhale453@users.noreply.github.com>
2022-11-21 13:00:35 -0800

    idk I tried to commit these last commit

--- newVar.py
+++ newVar.py
@@ -1,28 +1,3 @@
-"""
-# user variables
-
-print(user.username)
-print(user.playerClass)
-print(user.backpack)
-print(user.special)
-print(user.totalHealth)
-print(user.currentHealth)
-print(user.money)
-
-# user.weapon variables
-
-print(user.weapon.name)
-print(user.weapon.damage)
-print(user.weapon.price)
-print(user.weapon.rarity)
-
-# var variables
-
-print(version)
-print(rarities)
-print(checkPointSecret)
-print(validPlayerClasses)
-"""
 class weapon:
     def __init__(self, name, damage, price, rarity):
         self.name = name
@@ -36,45 +11,55 @@ rarities = ("common", "uncommon", "rare", "epic", "legendary", "mythic")
 checkpointSecret = False
 validPlayerClasses = ("wizard", "knight", "rogue", "armorer")
 
+# user variables
+username = "none"
+playerClass = "none"
+backpack = []
+special = "none"
+#weapon = weapon("none", 0, 0, rarities[0])
+totHealth = "none"
+curHealth = "none"
 
-# spaghetti code
-print("enter a username")
-username = input()
-print(
-"Wizard\n baseHealth = 80\n base damage = 1\n special = fireball\n"
-"Knight\n baseHealth = 110\n base damage = 2\n special = strike\n"
-"Rogue\n baseHealth = 50\n base damage = 3\n special = slash\n"
-"Armorer\n baseHealth = 150\n base damage = 1\n special = shield\n"
-)
-print("enter a class")
-while True:
-    playerClass = input()
-    playerClass.lower()
-    if playerClass in validPlayerClasses:
-        break
-    else:
-        print("invalid argument")
-if playerClass == "wizard":
-    backpack = ["glowing orb"]
-    special = "spells"
-    weapon = weapon("wooden staff", 5, 0, rarities[0])
-    totalHealth = 80
-    currentHealth = totalHealth
-elif playerClass == "knight":
-    backpack = ["religious relic"]
-    special = "strike"
-    weapon = weapon("wooden sword", 10, 0, rarities[0])
-    totalHealth = 110
-    currentHealth = totalHealth
-elif playerClass == "rogue":
-    backpack = ["silver fang"]
-    special = "slash"
-    weapon = weapon("wooden dagger", 15, 0, rarities[0])
-    totalHealth = 50
-    currentHealth = totalHealth
-elif playerClass == "armorer":
-    backpack = ["golden plate"]
-    special = "bash"
-    weapon = weapon("wooden shield", 5, 0, rarities[0])
-    totalHealth = 150
-    currentHealth = totalHealth
+def varAssign():
+    glob = globals()
+    # variable assign
+    print("enter a username")
+    username = input()
+    print(
+    "Wizard\n baseHealth = 80\n base damage = 1\n special = fireball\n"
+    "Knight\n baseHealth = 110\n base damage = 2\n special = strike\n"
+    "Rogue\n baseHealth = 50\n base damage = 3\n special = slash\n"
+    "Armorer\n baseHealth = 150\n base damage = 1\n special = shield\n"
+    )
+    print("enter a class")
+    while True:
+        playerClass = input()
+        playerClass = playerClass.lower()
+        if playerClass in validPlayerClasses:
+            break
+        else:
+            print("invalid argument")
+    if playerClass == "wizard":
+        glob['backpack'] = ["glowing orb"]
+        special = "spells"
+        #weapon = weapon("wooden staff", 5, 0, rarities[0])
+        totHealth = 80
+        curHealth = totHealth
+    elif playerClass == "knight":
+        backpack = ["religious relic"]
+        special = "strike"
+        #weapon = weapon("wooden sword", 10, 0, rarities[0])
+        totHealth = 110
+        curHealth = totHealth
+    elif playerClass == "rogue":
+        backpack = ["silver fang"]
+        special = "slash"
+        #weapon = weapon("wooden dagger", 15, 0, rarities[0])
+        totHealth = 50
+        curHealth = totHealth
+    elif playerClass == "armorer":
+        backpack = ["golden plate"]
+        special = "bash"
+        #weapon = weapon("wooden shield", 5, 0, rarities[0])
+        totHealth = 150
+        curHealth = totHealth
